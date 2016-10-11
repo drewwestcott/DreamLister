249424127206b4009f0742a11e47064947043f80
Drew Westcott <code@drewwestcott.co.uk>
2016-10-11 23:39:00 +0100

    Storyboard and Prototype cell set

---
+++ dreamLister2/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  dreamLister2
+//
+//  Created by Drew Westcott on 11/10/2016.
+//  Copyright © 2016 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
